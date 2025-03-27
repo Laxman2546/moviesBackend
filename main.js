69353aa774989a523dc1c9320fb9fb8f9e530f09
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-03-27 16:21:38 +0530

    tv added

--- main.js
+++ main.js
@@ -48,6 +48,25 @@ app.get("/search/movie/:query/:id", async (req, res) => {
     console.error("Error fetching data:", error);
   }
 });
+app.get("/search/tv/:query/:id", async (req, res) => {
+  const params = req.params;
+  console.log(params);
+  const url = `https://api.themoviedb.org/3/search/tv?query=${params.query}&page=${params.id}`;
+  try {
+    const data = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.VITE_APP_MOVIE_ACCESS_KEY}`,
+        Accept: "application/json",
+      },
+    });
+    const response = await data.json();
+
+    res.send(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
