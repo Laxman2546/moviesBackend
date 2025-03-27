34c441360b47a1deef23835d282267eee66cd896
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-03-27 17:01:59 +0530

    added all routes

--- main.js
+++ main.js
@@ -67,6 +67,63 @@ app.get("/search/tv/:query/:id", async (req, res) => {
     console.error("Error fetching data:", error);
   }
 });
+app.get("/webseries/:id", async (req, res) => {
+  const params = req.params;
+  console.log(params);
+  const url = `https://api.themoviedb.org/3/discover/tv?include_adult=false&with_original_language=te&page=${params.id}&sort_by=release_date.desc`;
+  try {
+    const data = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.VITE_APP_MOVIE_ACCESS_KEY}`,
+        Accept: "application/json",
+      },
+    });
+    const response = await data.json();
+
+    res.send(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
+app.get("/horror/:id", async (req, res) => {
+  const params = req.params;
+  console.log(params);
+  const url = `https://api.themoviedb.org/3/discover/movie?&with_genres=27&page=${params.id}`;
+  try {
+    const data = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.VITE_APP_MOVIE_ACCESS_KEY}`,
+        Accept: "application/json",
+      },
+    });
+    const response = await data.json();
+
+    res.send(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
+app.get("/anime/:id", async (req, res) => {
+  const params = req.params;
+  console.log(params);
+  const url = `https://api.themoviedb.org/3/discover/movie?with_genres=16&sort_by=popularity.desc&page=${params.id}`;
+  try {
+    const data = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.VITE_APP_MOVIE_ACCESS_KEY}`,
+        Accept: "application/json",
+      },
+    });
+    const response = await data.json();
+
+    res.send(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
